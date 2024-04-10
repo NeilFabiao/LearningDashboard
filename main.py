630f44880c5935b9f95af671f15cfc97f97117c0
neilfabiao <50138381+NeilFabiao@users.noreply.github.com>
2024-04-10 15:10:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -63,11 +63,11 @@ col1, col2 = st.columns(2)
 with col1:
     predicted_value = float(prediction[0])
     st.write(f"The median house value is : ${predicted_value * 100000:,.2f}")
-    #st.write('---')
+    st.write('---')
 
 # Map Visualization
 with col2:
-    #st.subheader('Geographical Distribution of Predicted House Prices')
+    st.subheader('Geographical Distribution of Predicted House Prices')
     fig = px.scatter_mapbox(
         df,
         lat="Latitude",
