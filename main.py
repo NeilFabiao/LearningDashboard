f58b69fb184aa19dc1020702c83c56ee714692aa
neilfabiao <50138381+NeilFabiao@users.noreply.github.com>
2024-04-10 14:23:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from sklearn.tree import DecisionTreeRegressor
 #from sklearn.ensemble import RandomForestRegressor
 import matplotlib.pyplot as plt
 import plotly.express as px  
-st.set_option('deprecation.showPyplotGlobalUse', False)
 
 
 st.write("""
