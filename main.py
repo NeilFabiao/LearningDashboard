57d12f12757380b952cdbc8a5f1cc2609fb6e291
neilfabiao <50138381+NeilFabiao@users.noreply.github.com>
2024-04-10 15:24:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -69,16 +69,16 @@ col1, col2 = st.columns(2)
 with col1:
     
     st.subheader('Geographical Distribution of House Prices')
-        fig = px.scatter_mapbox(
-            pd.concat([X, pd.DataFrame({'HousePrice': Y})], axis=1),
-            lat="Latitude",
-            lon="Longitude",
-            color="HousePrice",
-            color_continuous_scale=px.colors.cyclical.IceFire,
-            zoom=5,
-            mapbox_style="carto-positron"
-        )
-        st.plotly_chart(fig, use_container_width=True)
+    fig = px.scatter_mapbox(
+        pd.concat([X, pd.DataFrame({'HousePrice': Y})], axis=1),
+        lat="Latitude",
+        lon="Longitude",
+        color="HousePrice",
+        color_continuous_scale=px.colors.cyclical.IceFire,
+        zoom=5,
+        mapbox_style="carto-positron"
+    )
+    st.plotly_chart(fig, use_container_width=True)
 
 # Column 2: 
 with col2:
